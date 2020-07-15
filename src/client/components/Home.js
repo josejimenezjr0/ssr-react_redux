6a7fac34cf4b242d49f05f9f5bd49af4468f113c
Jose Jimenez Jr <josejimenezjr0@gmail.com>
2020-07-15 16:11:58 -0500

    Add client side js init

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,5 +1,14 @@
 import React from 'react'
 
-const Home = () => <div>I'm the home componenet</div>
+const Home = () => {
+  return (
+  <div>
+    <div>I'm the super cool home componenet</div>  
+    <button onClick={() => console.log('Hi there!')}>Press me!</button>
+  </div>
+  
+  )
+}
+
 
 export default Home
